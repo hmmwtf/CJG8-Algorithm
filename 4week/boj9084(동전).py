0d8c2c384047e0f5b7e6e12f2f732336528d2fc5
hmmwtf <hmmwtf@users.noreply.github.com>
2025-04-07 00:08:29 +0900

    0407 DP

--- 4week/boj9084(동전).py
+++ 4week/boj9084(동전).py
@@ -7,3 +7,12 @@ for _ in range(t):
     n = int(input().strip())
     coins = list(map(int, input().strip().split()))
     money = int(input().strip())
+    
+    dp = [0] * (money + 1)
+    dp[0] = 1
+    
+    for coin in coins:
+        for x in range(coin, money + 1):
+            dp[x] += dp[x-coin]
+    
+    print(dp[money])
